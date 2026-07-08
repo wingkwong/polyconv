f5d2162493a33b6c623b79a5e26538738801f4e6
WK Wong <wingkwong.code@gmail.com>
2026-07-08 19:57:46 +0800

    chore(deps): bump versions

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -8,7 +8,7 @@ const program = new Command();
 program
   .name("polyconv")
   .description("A modular toolkit for converting between data formats")
-  .version("0.1.0");
+  .version("0.1.1");
 
 // Add commands
 program.addCommand(createConvertCommand());
